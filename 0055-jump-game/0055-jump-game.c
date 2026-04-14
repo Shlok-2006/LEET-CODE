d09ba90ec3b11748b26c0df51e65c46e62813b64
Shlok Agarwal <shlokagarwal0806@gmail.com>
2026-04-15 00:14:57 +0530

    Time: 0 ms (100%), Space: 11.4 MB (26.5%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.c
@@ -0,0 +1,15 @@
+/*
+Greedy
+Time: O(n)
+*/
+bool canJump(int* nums, int numsSize) {
+    int reach = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        if (i > reach) return false;
+        if (i + nums[i] > reach)
+            reach = i + nums[i];
+    }
+
+    return true;
+}
