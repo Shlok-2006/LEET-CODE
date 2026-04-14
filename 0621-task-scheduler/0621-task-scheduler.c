8d1f0111b120f64da11f77a731225bd3d66eff3c
Shlok Agarwal <shlokagarwal0806@gmail.com>
2026-04-15 00:08:35 +0530

    Time: 0 ms (100%), Space: 9.5 MB (68.19%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.c
@@ -0,0 +1,29 @@
+/*
+Greedy counting frequencies
+Time: O(n)
+*/
+
+int leastInterval(char* tasks, int tasksSize, int n) {
+    int freq[26] = {0};
+
+    for (int i = 0; i < tasksSize; i++)
+        freq[tasks[i] - 'A']++;
+
+    int maxFreq = 0;
+    for (int i = 0; i < 26; i++)
+        if (freq[i] > maxFreq)
+            maxFreq = freq[i];
+
+    int maxCount = 0;
+    for (int i = 0; i < 26; i++)
+        if (freq[i] == maxFreq)
+            maxCount++;
+
+    int partCount = maxFreq - 1;
+    int partLength = n - (maxCount - 1);
+    int emptySlots = partCount * partLength;
+    int availableTasks = tasksSize - maxFreq * maxCount;
+    int idles = emptySlots > availableTasks ? emptySlots - availableTasks : 0;
+
+    return tasksSize + idles;
+}
